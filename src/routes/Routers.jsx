36f6dc47d3f31adbe7362dfbc1598cc8ca68b973
Dineth Dilshan <E2145112@bit.uom.lk>
2024-01-07 21:59:57 +0530

    Add Some changes in routes file

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -4,14 +4,15 @@ import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Services from "../pages/Services";
 import Singup from "../pages/Singup";
-import Doctors from "../pages/Doctors";
-import DoctorDetails from "../pages/DoctorDetails";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
 
 import {Routes, Route} from 'react-router-dom';
 
  const Routers = () => {
    return (
      <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/loging" element={<Login/>} />
